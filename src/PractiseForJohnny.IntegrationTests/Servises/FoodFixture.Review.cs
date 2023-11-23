d4205076f9914f63d63a15587f5dc21523fcf2de
JOHNNY.X <2546088435@qq.com>
2023-11-24 07:26:19 +0800

    update

--- src/PractiseForJohnny.IntegrationTests/Servises/FoodFixture.Review.cs
+++ src/PractiseForJohnny.IntegrationTests/Servises/FoodFixture.Review.cs
@@ -17,42 +17,47 @@ public partial class FoodFixture
 
         var food = new UpdateFoodDto { Id = 11, Name = "mike", Color = "white" };
 
-        var beforeUpdateFood = await Run<IRepository, Foods>(async repository =>
-            await repository.GetByIdAsync<Foods>(11).ConfigureAwait(false));
+        await Run<IRepository>(async repository =>
+        {
+            var beforeUpdateFood = await repository.GetByIdAsync<Foods>(11).ConfigureAwait(false);
 
-        beforeUpdateFood.Id.ShouldBe(11);
-        beforeUpdateFood.Name.ShouldBe("cake");
-        beforeUpdateFood.Color.ShouldBe("red");
+            beforeUpdateFood.Id.ShouldBe(11);
+            beforeUpdateFood.Name.ShouldBe("cake");
+            beforeUpdateFood.Color.ShouldBe("red");
 
-        await _foodsUtil.UpdateFood(food);
+            await _foodsUtil.UpdateFood(food);
 
-        var afterUpdateFood = await Run<IRepository, Foods>(async repository =>
-            await repository.GetByIdAsync<Foods>(food.Id).ConfigureAwait(false));
+            var afterUpdateFood = await Run<IRepository, Foods>(async repository =>
+                await repository.GetByIdAsync<Foods>(food.Id).ConfigureAwait(false));
 
-        afterUpdateFood.Id.ShouldBe(11);
-        afterUpdateFood.Name.ShouldBe("mike");
-        afterUpdateFood.Color.ShouldBe("white");
+            afterUpdateFood.Id.ShouldBe(11);
+            afterUpdateFood.Name.ShouldBe("mike");
+            afterUpdateFood.Color.ShouldBe("white");
+        });
     }
 
     [Fact]
     public async Task CanDeleteFood()
     {
         await RunWithUnitOfWork<IRepository>(async repository =>
-            await repository.InsertAsync<Foods>(new Foods { Id = 11, Name = "cake", Color = "red" }).ConfigureAwait(false));
-        
+            await repository.InsertAsync<Foods>(new Foods { Id = 11, Name = "cake", Color = "red" })
+                .ConfigureAwait(false));
+
         var food = new DeleteFoodDto() { Id = 11 };
 
-        var beforeDeleteFood = await Run<IRepository, Foods>(async respository =>
-            await respository.GetByIdAsync<Foods>(food.Id));
+        await Run<IRepository>(async respository =>
+        {
+            var beforeDeleteFood = await respository.GetByIdAsync<Foods>(food.Id);
 
-        beforeDeleteFood.Id.ShouldBe(11);
+            beforeDeleteFood.Id.ShouldBe(11);
 
-        await _foodsUtil.DeleteFood(food);
+            await _foodsUtil.DeleteFood(food);
 
-        var afterDeleteFood = await Run<IRepository, Foods>(async respository =>
-            await respository.GetByIdAsync<Foods>(food.Id));
+            var afterDeleteFood = await Run<IRepository, Foods>(async respository =>
+                await respository.GetByIdAsync<Foods>(food.Id));
 
-        afterDeleteFood.ShouldBeNull();
+            afterDeleteFood.ShouldBeNull();
+        });
     }
 
     [Fact]
