a60085ddb9fd361c4d5ee2af187cb1bbbd5a3d31
weixiao <dengshi@ifca.com.cn>
2024-03-01 11:45:59 +0800

    配置需要授权的 Swagger

--- src/PractiseForJohnny.Api/Startup.cs
+++ src/PractiseForJohnny.Api/Startup.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using Hangfire;
 using Microsoft.OpenApi.Models;
+using MySqlX.XDevAPI;
 using PractiseForJohnny.Core.Extension;
 using PractiseForJohnny.Core.Hangfire;
 using PractiseForJohnny.Core.Service;
@@ -20,43 +21,71 @@ public class Startup
     {
         services.AddJWT(Configuration);
 
+        services.AddMvc();
+
         services.AddControllers();
-        
+
         services.AddHangFireService(Configuration);
-        
+
+        //services.AddSwaggerGen(c =>
+        //{
+        //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PractiseForJohnny", Version = "v1" });
+        //});
+        // 配置需要授权的 Swagger
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "PractiseForJohnny", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Description = "Bearer Token",
+                Name = "Authorization",
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference=new OpenApiReference()
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },new string[]{ }
+                }
+            });
         });
     }
-    
+
     public void ConfigureContainer(ContainerBuilder builder)
     {
         builder.RegisterModule(new PractiseForJohnnyModule(Configuration, typeof(PractiseForJohnnyModule).Assembly));
     }
-    
+
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
-        
+
         app.UseRouting();
-        
+
         app.UseAuthentication();
 
         app.UseAuthorization();
 
         app.UseHangfireServer();
         app.UseHangfireDashboard();
-        
+
         app.UseSwagger();
         app.UseSwaggerUI(c =>
         {
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         });
-        
+
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
